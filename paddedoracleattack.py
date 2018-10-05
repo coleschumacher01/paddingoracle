848868c8c401a064f039d27c76b231e478ee1e75
Cole <coleschumacher@gmail.com>
2018-10-05 02:44:51 -0400

    implemented the increment method for oracle attack

--- paddedoracleattack.py
+++ paddedoracleattack.py
@@ -3,11 +3,36 @@ import cbc
 import paddingoracle
 import sys
 
+def hexincrement(c):
+
+    currrentspot = ''
+
+    if c.isdigit() && int(c) < 9:
+        currentspot = str(int(c) + 1)
+    elif c.isdigit():
+        currentspot = 'a'
+    elif currentspot = 'a':
+        currentspot = 'b'
+    elif currentspot = 'b':
+        currentspot = 'c'
+    elif currentspot = 'c':
+        currentspot = 'd'
+    elif currentspot = 'd':
+        currentspot = 'e'
+    elif currentspot = 'e':
+        currentspot = 'f'
+    else currentspot = 'f':
+        currentspot = '0'
+    return currrentspot
+
+
 def incrementNextPad(s, current):
     currentspot = s[len(s) - 2*(current + 1): len(s) - current*2]
-
-    #increment the currentspot here
-
+    
+    newval = hexincrement(currentspot[1])
+    currentspot[1] = newval
+    if newval == '0':
+        currentspot[0] = hexincrement(currentspot[0])
     return s[:len(s) - 2*(current + 1)] + currentspot + s[len(s) - current*2:]
 
 #checks to see how much of the block is corectly padded
